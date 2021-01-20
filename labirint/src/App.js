b758447264190c0bdcde30b97b650b4924ba9ca2
cootook <cootook@gmail.com>
2021-01-20 21:48:33 +0300

    feat: add menu and footer

--- labirint/src/App.js
+++ labirint/src/App.js
@@ -1,9 +1,16 @@
 import './components/js/canvas'
 import './App.css';
+import './components/css/page.scss';
+import './components/css/menu.scss';
+import './components/css/footer.scss';
+import Menu from './components/js/Menu';
+import Footer from './components/js/Footer';
 
 function App() {
   return (
-    <div className="App">
+    <div className="wrapper">
+      <Menu />
+      <Footer />
     </div>
   );
 }
